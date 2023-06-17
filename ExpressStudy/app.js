3109e29d67a105bc6d06996ecd89f2832c2c9549
younhoso <thdbsgh3443@naver.com>
2023-06-17 15:28:26 +0900

    express 기본기

--- ExpressStudy/app.js
+++ ExpressStudy/app.js
@@ -22,7 +22,7 @@ app.get('/api/members/:id', (req, res) => {
   if(member){
     res.send(member);
   } else {
-    res.status(404).send({ message : 'There is no such member' });
+    res.status(404).send({ message : 'There is no member with the id!' });
   }
 });
 
@@ -30,6 +30,32 @@ app.post('/api/members', (req, res) => {
   const newMember = req.body;
   members.push(newMember);
   res.send(newMember)
+});
+
+app.put('/api/members/:id', (req, res) => {
+  const { id } = req.params;
+  const newInfo = req.body;
+  const member = members.find((m) => m.id === Number(id));
+  if(member){
+    Object.keys(newInfo).forEach((prop) => {
+      console.log(newInfo)
+      member[prop] = newInfo[prop];
+    });
+    res.send(member);
+  } else {
+    res.status(404).send({message: 'There is no member with the id!!'})
+  }
+});
+
+app.delete('/api/members/:id', (req, res) => {
+  const { id } = req.params;
+  const membersCount = members.length;
+  members = members.filter((member) => member.id !== Number(id));
+  if(members.length < membersCount){
+    res.send({message: 'Deleted'});
+  } else {
+    res.status(404).send({message: 'There is no member with the id!'})
+  }
 });
 
 app.listen(3000, () => {
